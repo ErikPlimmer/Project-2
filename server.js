5351a19330f7b2e6e83aba2500ccb4558c1d9ad0
Erik Plimmer <Elp013@Gmail.com>
2018-03-26 18:33:09 -0400

    update

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser"); 
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(__dirname + "/app/public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+require("./app/routing/api-routes")(app);
+require("./app/routing/html-routes")(app);
+
+
+
+
+const port = process.env.PORT || process.argv[2] || 80;
+
+app.listen(port, function() { 
+        console.log(`Started server on  ${port}...`); 
+});
+
+
+  module.exports = app;
+
+
