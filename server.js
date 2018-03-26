86a37085feedf2f136d0c482a4c275475ffc68ed
Erik Plimmer <Elp013@Gmail.com>
2018-03-26 19:36:54 -0400

    update server is working

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser"); 
 const path = require("path");
 
+var db = require("./models");
 const app = express();
 
 app.use(express.static(__dirname + "/app/public"));
@@ -10,19 +11,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-require("./app/routing/api-routes")(app);
-require("./app/routing/html-routes")(app);
-
+require("./app/routes/api-routes");
+require("./app/routes/html-routes");
 
 
 
 const port = process.env.PORT || process.argv[2] || 80;
 
-app.listen(port, function() { 
-        console.log(`Started server on  ${port}...`); 
+// Syncing our database and logging a message to the user upon success
+db.sequelize.sync().then(function() {
+  app.listen(port, function() {
+    console.log(`Started server on  ${port}...`);
+  });
 });
-
-
-  module.exports = app;
 
 
