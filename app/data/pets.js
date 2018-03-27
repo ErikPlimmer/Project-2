9361596d2dd834258e2046778944a47b41d91513
Ashley Mackenzie <shannon.ashley.mackenzie@gmail.com>
2018-03-26 21:34:15 -0400

    updates

--- app/data/pets.js
+++ app/data/pets.js
@@ -12,25 +12,31 @@ var pets = [{
 },
 {
   "type": "Cat",
-  "image": "",
+  "image": "https://cdn.suwalls.com/wallpapers/animals/posing-cats-53888-1920x1200.jpg",
   "description":,
   "scores":[]
 },
 {
   "type": "Reptile",
-  "image": "",
+  "image": "http://media.mlive.com/news/baycity_impact/photo/jvc-cam-july-2011-445jpg-9cb0401259fdae52.jpg",
   "description":,
   "scores":[]
 },
 {
   "type": "Rodent"
-  "image": "",
+  "image": "https://static.rcgroups.net/forums/attachments/5/7/2/3/8/9/a8673905-133-hamster-thumbs-up-cute-hamters-animal-picture-pics.jpg",
   "description":,
   "scores":[]
 },
 {
   "type": "Fish",
-  "image": "",
+  "image": "https://whenyouriseup.com/wp-content/uploads/2017/02/goldfish.jpg",
+  "description":,
+  "scores":[]
+},
+{
+  "type": "Bird",
+  "image": "http://www.zooniversity.org/images/animals/Moluccan-Cockatoo.jpg",
   "description":,
   "scores":[]
 },
@@ -42,7 +48,7 @@ var pets = [{
 },
 {
   "type": "Any pet you want",
-  "image": "",
+  "image": "https://www.unlitips.com/wp-content/uploads/2016/10/Pets-at-home-where-pets-come-first.jpg",
   "description":,
   "scores":[]
 }]
