3c5f20bc227546b8df5c9d0231474290bd09ca08
Ashley Mackenzie <shannon.ashley.mackenzie@gmail.com>
2018-03-26 21:03:56 -0400

    updated main page, created survey html, and started survey js

---
+++ app/data/pets.js
@@ -0,0 +1,48 @@
+var pets = [{
+  "type": "Small dog",
+  "image": "https://i.pinimg.com/736x/11/d4/8a/11d48a6e03d40d611ad220600dbe7724--small-breed-dogs-pet-dogs.jpg",
+  "description":"",
+  "scores":[]
+},
+{
+  "type": "Large dog",
+  "image": "https://i.ytimg.com/vi/X4rWauONg70/maxresdefault.jpg",
+  "description":,
+  "scores":[]
+},
+{
+  "type": "Cat",
+  "image": "",
+  "description":,
+  "scores":[]
+},
+{
+  "type": "Reptile",
+  "image": "",
+  "description":,
+  "scores":[]
+},
+{
+  "type": "Rodent"
+  "image": "",
+  "description":,
+  "scores":[]
+},
+{
+  "type": "Fish",
+  "image": "",
+  "description":,
+  "scores":[]
+},
+{
+  "type": "You don't need a pet",
+  "image": "",
+  "description":,
+  "scores":[]
+},
+{
+  "type": "Any pet you want",
+  "image": "",
+  "description":,
+  "scores":[]
+}]
